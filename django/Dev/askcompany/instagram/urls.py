098f98bd7712b834844292869c3bd963f933419b
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-09-22 16:58:12 +0900

    List View

--- django/Dev/askcompany/instagram/urls.py
+++ django/Dev/askcompany/instagram/urls.py
@@ -1,5 +1,9 @@
-
+from django.urls import path, re_path
+from . import views
 
 urlpatterns = [
-    
+    path('', views.post_list),
+    path('<int:pk>/', views.post_detail), # pk가 int로 넘어온다.
+    # re_path(r'(?P<pk>\d+)/$', views.post_detail) 위의 path와 같은 역할이지만 문자열로 pk가 넘어온다.
+    # '<int..>' 이 부분을 Converter라고 부른다
 ]
--- django/Dev/askcompany/instagram/views.py
+++ django/Dev/askcompany/instagram/views.py
@@ -1,3 +1,21 @@
+from django.views.generic import ListView
 from django.shortcuts import render
-
+from .models import Post
+from django.http import HttpResponse
 # Create your views here.
+
+post_list = ListView.as_view(model=Post)
+
+# def post_list(request):
+#     qs = Post.objects.all()
+#     q= request.GET.get('q','')
+#     if q:
+#         qs = qs.filter(message__icontains=q)
+#         return render(request, 'instagram/post_list.html', {
+#             'post_list': qs,
+#             'q' :q,
+#         })
+
+def post_detail(request : HttpResponse, pk: int) -> HttpResponse: # type을 쓰는 쪽으로 유행이 가고있다
+    response = HttpResponse()
+    return response
