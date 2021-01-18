937dfa73fa2159900f6fad2d0915ae175a9cc166
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2021-01-18 23:23:53 +0900

    feat: implement base

--- django/blogproject/blogapp/urls.py
+++ django/blogproject/blogapp/urls.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+app_name = "blogapp"
+
 urlpatterns = [
     path('', views.home, name='home'),
+    path('fbv/', views.fbv, name='fbv'),
+    path('cbv/', views.cbv, name='cbv'),
 ]
--- django/blogproject/blogapp/views.py
+++ django/blogproject/blogapp/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'blogapp/home.html', {})
+
+def fbv(request):
+    return render(request, 'blogapp/fbvbase.html',{})
+
+def cbv(request):
+    return render(request, "blogapp/cbvbase.html", {})
